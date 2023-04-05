27d0ffd013ebc3185498bea68459e40375c4357b
Joao Vitor <100004933+Jao1br@users.noreply.github.com>
2023-04-05 20:58:56 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+
+
+function pergunta(){
+    
+    window.prompt("Qual seu nome")
+    window.prompt("Qual seu carro?")
+    window.confirm("Clique no sim caso seu carro seja automático caso contrário cancelar")
+    
+    }
+    pergunta(onclick())
